c31dbee894d3b2d034ce546a43ace4584a148074
s0ulcats <babich.danya30@gmail.com>
2025-03-21 12:42:36 +0200

    add react suspense

--- src/components/features/main/MainPage.tsx
+++ src/components/features/main/MainPage.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import MealCard from "@/components/ui/elements/MealCard";
 import PaginationComponent from "@/components/ui/elements/PaginationComponent";
 import { useSearchParams } from "next/navigation";
 import CategoryFilter from "@/components/ui/elements/CategoryFilter";
+import { Skeleton } from "@/components/ui/common/Skeleton";
 
 interface Meal {
   idMeal: number
@@ -107,6 +108,21 @@ export default function MainPage() {
   const categories = ["All", ...new Set(meals.map(meal => meal.strCategory))];
 
   return (
+    <Suspense fallback={
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
+        {/* Скелетон для каждого элемента */}
+        {[...Array(itemsPerPage)].map((_, index) => (
+          <div key={index} className="w-[250px] h-[350px] rounded-3xl bg-primary p-4 shadow-lg text-white relative flex flex-col justify-between">
+            <Skeleton className="w-full h-40 rounded-lg" />
+            <div className="text-center mt-4">
+              <Skeleton className="w-3/4 h-6 mx-auto" />
+              <Skeleton className="w-1/2 h-4 mx-auto mt-2" />
+            </div>
+            <Skeleton className="w-full h-10 rounded-full mt-4" />
+          </div>
+        ))}
+      </div>
+    }>
     <div className="p-6">
       <CategoryFilter
         categories={categories}
@@ -126,5 +142,6 @@ export default function MainPage() {
         setCurrentPage={setCurrentPage}
       />
     </div>
+    </Suspense>
   );
 }
